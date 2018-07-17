b226fa0ced501d1456089e8912e146b675160add
Aditya Shukla <aditya@Adityas-MacBook-Pro.local>
2018-07-17 23:12:20 +0530

    added customer list and model logic

--- frontend/src/app/customer-list/customer-list.component.ts
+++ frontend/src/app/customer-list/customer-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-customer-list',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer-list.component.css']
 })
 export class CustomerListComponent implements OnInit {
+  public columns = ['id', 'name'];
+  public rows;
 
-  constructor() { }
+  constructor(public apiService: ApiService) { }
 
   ngOnInit() {
+    this.apiService.get("customers").subscribe((data) => {
+      console.log(data);
+      this.rows = data;
+    });
   }
 
 }
---
+++ frontend/src/app/customer-list/customer.model.ts
@@ -0,0 +1,5 @@
+export class Customer{
+    public id:number;
+    public name:string;
+
+}
