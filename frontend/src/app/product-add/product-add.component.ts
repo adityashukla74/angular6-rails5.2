4026ce26d58494021ee96c4fb1825124866ac0a1
Aditya Shukla <aditya@Adityas-MacBook-Pro.local>
2018-07-17 23:20:11 +0530

    product-add logic

--- frontend/src/app/product-add/product-add.component.ts
+++ frontend/src/app/product-add/product-add.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Product } from '../product-list/product.model'
+import { ApiService } from '../api.service';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-product-add',
   templateUrl: './product-add.component.html',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductAddComponent implements OnInit {
 
-  constructor() { }
+  public product: Product = new Product();
+  constructor(public apiService: ApiService, public actRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    this.actRoute.params.subscribe((data: any) => {
+      alert(data.id);
+      if (data && data.id) {
+        this.apiService.get("products/" + data.id).subscribe((data: Product) => {
+          this.product = data;
+        })
+      }
+      else {
+        this.product = new Product();
+      }
+    })
   }
-
 }
