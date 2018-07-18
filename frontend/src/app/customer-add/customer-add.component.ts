64786d18a6ddf347b4feafda5fc871ae5e2d425d
Aditya Shukla <aditya@Adityas-MacBook-Pro.local>
2018-07-18 14:30:04 +0530

    Angular Ivy Support

--- frontend/src/app/customer-add/customer-add.component.ts
+++ frontend/src/app/customer-add/customer-add.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-customer-add',
-  templateUrl './customer-add.component.html',
+  templateUrl: './customer-add.component.html',
   styleUrls: ['./customer-add.component.css']
 })
 export class CustomerAddComponent implements OnInit {
